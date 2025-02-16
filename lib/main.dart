0a8a0e87a81ebc783cfd39f4d405a336bd542e1f
mdmamunurrashidhridoy <rashid15-5273@diu.edu.bd>
2025-02-16 10:34:09 +0600

    task02

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:iconsax_flutter/iconsax_flutter.dart';
+// import 'package:iconsax_flutter/iconsax_flutter.dart';
 
 void main() {
   runApp(
@@ -8,37 +8,41 @@ void main() {
         appBar: AppBar(
           title: Text("Flutter Row Lab"),
         ),
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(
-              size: 70,
-              Icons.star_rounded
-            ),
-            Icon(
-              size: 70,
-              Icons.star_rounded
-            ),
-            Icon(
-              size: 70,
-              Icons.star_rounded
-            ),
-            Icon(
-              size: 70,
-              Icons.star_rounded
-            ),
-            // Text(
-            //   "Hridoy",
-            //   style: TextStyle(
-            //     color: Colors.amber,
-            //     fontSize: 23
-            //   ),
-            // ),
-            Icon(
-              size: 70,
-              Icons.star_border
-            ),
-          ],
+        body: Container(
+          height: double.infinity,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              
+              Icon(
+                size: 70,
+                Icons.star_rounded
+              ),
+              Icon(
+                size: 70,
+                Icons.star_rounded
+              ),
+              Icon(
+                size: 70,
+                Icons.star_rounded
+              ),
+              Icon(
+                size: 70,
+                Icons.star_rounded
+              ),
+              // Text(
+              //   "Hridoy",
+              //   style: TextStyle(
+              //     color: Colors.amber,
+              //     fontSize: 23
+              //   ),
+              // ),
+              Icon(
+                size: 70,
+                Icons.star_border
+              ),
+            ],
+          ),
         ),
       ),
     )
